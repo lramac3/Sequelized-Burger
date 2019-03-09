3c9972d2cac0788168aaeb5eb039fdad0747bd29
Linu Ramachandran <linuram@gmail.com>
2019-03-09 03:02:01 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-let express = require("express");
-let bodyParser = require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 let PORT = process.env.PORT || 8080;
 
 let app = express();
+
+// Requiring our models for syncing
+const db = require("./models");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -14,19 +17,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
 
-// Set Handlebars.
-let exphbs = require("express-handlebars");
+// Set Handlebars
+const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-let routes = require("./controllers/burgers_controller.js");
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
 
-app.use("/",routes);
-
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
 });
+
+
