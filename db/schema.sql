fb91bf5ffeb4cb96a5682323a273a8da70a79ea7
Linu Ramachandran <linuram@gmail.com>
2019-03-09 02:56:35 -0500

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
 
-CREATE DATABASE bazzny0d60g4149x;
-USE bazzny0d60g4149x;
+CREATE DATABASE sei4ysj76n7s5wv0;
+-- USE sei4ysj76n7s5wv0;
 
 
-CREATE TABLE burgers(
-    id INTEGER AUTO_INCREMENT NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    ate BOOLEAN DEFAULT false,
-    liked BOOLEAN DEFAULT false,
-    PRIMARY KEY(id)
-);
+-- CREATE TABLE burgers(
+--     id INTEGER AUTO_INCREMENT NOT NULL,
+--     name VARCHAR(50) NOT NULL,
+--     ate BOOLEAN DEFAULT false,
+--     liked BOOLEAN DEFAULT false,
+--     PRIMARY KEY(id)
+-- );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-USE bazzny0d60g4149x;
-INSERT INTO burgers(name) VALUES
-("Angus Burger"),("Butter Burger"),("Buffalo Burger"),("California Burger"),("Carolina Burger"),("Hawaii Burger"),
-("Kimchi Burger"),("Naan Burger"),("Veggie Burger");
+USE sei4ysj76n7s5wv0;
+-- INSERT INTO burgers(name) VALUES
+-- ("Angus Burger"),("Butter Burger"),("Buffalo Burger"),("California Burger"),("Carolina Burger"),("Hawaii Burger"),
+-- ("Kimchi Burger"),("Naan Burger"),("Veggie Burger");
