f4b042cd9e86fd3908d1da41e158d84df8e91ded
Linu Ramachandran <linuram@gmail.com>
2019-03-28 16:35:10 -0400

    updated burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,36 +1,25 @@
-// Import the ORM to create functions that will interact with the database.
-const orm = require("../config/orm.js");
-
-let burger = {
-    all: function (cb) {
-        //calls orm's .selectAll()
-        orm.selectAll("burgers", function (res) {
-            cb(res);
-        });
+module.exports = function(sequelize, DataTypes) {
+ let burger = sequelize.define("burger", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-
-    // The variables cols and vals are arrays.
-    //calls orm's .insertOne()
-    create: function (cols, vals, cb) {
-        orm.insertOne("burgers", cols, vals, function (res) {
-            cb(res);
-        });
+    visited: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
-
-    //calls orm's .updateOne()
-    update: function (objColVals, condition, cb) {
-        orm.updateOne("burgers", objColVals, condition, function (res) {
-            cb(res);
-        });
+    liked: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
+  });
 
-    //calls orm's .deleteOne()
-    delete: function (objColVals, cb) {
-        orm.deleteOne("burgers", objColVals, function (res) {
-            cb(res);
-        });
-    }
+  burger.associate = function(models) {
+    models.burger.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false,
+      },
+    });
+  };
+  return burger;
 };
-
-// Export the database functions for the controller (burgers_controller.js).
-module.exports = burger;
