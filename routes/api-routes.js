97f9373b2f0280a4810baea7beb25cde558a34c6
Linu Ramachandran <linuram@gmail.com>
2019-03-30 09:06:58 -0400

    Revert "updated routes"
    This reverts commit 22c9f0a19e14fa39b60daf8d3da9b0562817c607.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,5 @@
 // *********************************************************************************
-// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// api-routes.js file offers a set of routes for displaying and saving data to the db
 // *********************************************************************************
 
 // Dependencies
@@ -12,47 +12,53 @@ var db = require("../models");
 // =============================================================
 module.exports = function(app) {
   app.get("/", function(req, res) {
-    db.Restaurant.findAll({
-      include: [{ model: db.User, attributes: ["username"] }],
-      order: [[db.User, "username", "ASC"], ["name"]],
-    }).then(function(data) {
-      var hbsObject = {
-        restaurants: data,
-      };
-      db.User.findAll({}).then(function(data2) {
-        hbsObject["users"] = data2;
-        res.render("index", hbsObject);
+    db.burger
+      .findAll({
+        include: [{ model: db.User, attributes: ["username"] }],
+        order: [[db.User, "username", "ASC"], ["name"]],
+      })
+      .then(function(data) {
+        var hbsObject = {
+          burgers: data,
+        };
+        db.User.findAll({}).then(function(data2) {
+          hbsObject["users"] = data2;
+          res.render("index", hbsObject);
+        });
       });
-    });
   });
 
-  //root route, show each restaurant's information, calls model's .all()
-  app.get("/restaurants/:userid", function(req, res) {
-    db.Restaurant.findAll({
-      where: {
-        UserId: req.params.userid,
-      },
-      include: [{ model: db.User }],
-      order: [["name", "ASC"]],
-    }).then(function(data) {
-      var hbsObject = {
-        restaurants: data,
-      };
-      db.User.findAll({}).then(function(data2) {
-        hbsObject["users"] = data2;
-        res.json(hbsObject);
+  //root route, show each burger's information, calls model's .all()
+  app.get("/burgers/:userid", function(req, res) {
+    db.burger
+      .findAll({
+        where: {
+          UserId: req.params.userid,
+        },
+        include: [{ model: db.User }],
+        order: [["name", "ASC"]],
+      })
+      .then(function(data) {
+        var hbsObject = {
+          burgers: data,
+        };
+        db.User.findAll({}).then(function(data2) {
+          hbsObject["users"] = data2;
+          res.json(hbsObject);
+        });
       });
-    });
   });
 
   //when post request comes from client, calls model's create()
-  app.post("/api/restaurants", function(req, res) {
-    db.Restaurant.create({
-      name: req.body.name,
-      UserId: req.body.userId,
-    }).then(function(data) {
-      res.json({ id: data.insertId });
-    });
+  app.post("/api/burgers", function(req, res) {
+    db.burger
+      .create({
+        name: req.body.name,
+        UserId: req.body.userId,
+      })
+      .then(function(data) {
+        res.json({ id: data.insertId });
+      });
   });
 
   //when post request comes from client, calls model's create()
@@ -65,40 +71,44 @@ module.exports = function(app) {
   });
 
   //when put request comes from client, calls model's update()
-  app.put("/api/restaurants/:id", function(req, res) {
-    db.Restaurant.update(
-      {
-        eaten: req.body.eaten,
-        liked: req.body.liked,
-      },
-      {
-        where: {
-          id: req.params.id,
+  app.put("/api/burgers/:id", function(req, res) {
+    db.burger
+      .update(
+        {
+          eaten: req.body.eaten,
+          liked: req.body.liked,
         },
-      }
-    ).then(function(data) {
-      if (data.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-    });
+        {
+          where: {
+            id: req.params.id,
+          },
+        }
+      )
+      .then(function(data) {
+        if (data.changedRows == 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        } else {
+          res.status(200).end();
+        }
+      });
   });
 
   //when delete request comes from client, calls model's delete()
-  app.delete("/api/restaurants/:id", function(req, res) {
-    db.Restaurant.destroy({
-      where: {
-        id: req.params.id,
-      },
-    }).then(function(data) {
-      if (data.affectedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-    });
+  app.delete("/api/burgers/:id", function(req, res) {
+    db.burger
+      .destroy({
+        where: {
+          id: req.params.id,
+        },
+      })
+      .then(function(data) {
+        if (data.affectedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        } else {
+          res.status(200).end();
+        }
+      });
   });
 };
